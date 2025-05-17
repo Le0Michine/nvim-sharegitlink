2f8d99e9307646ef24786fa330ef731a8b368daf
Lev Mishin <mishinl@amazon.com>
2025-05-17 10:42:01 -0700

    feat: initial commit

---
+++ lua/init.lua
@@ -0,0 +1,54 @@
+local utils = require "nvim-sharegitlink.utils"
+
+local config = {
+	link_builder = nil,
+}
+
+
+local ShareGitLink = {}
+
+function ShareGitLink.copy_gitfarm_link()
+	local target_path
+	if utils.is_directory_buffer() then
+		-- Tree mode: resolve path under cursor
+		target_path = utils.get_selected_tree_path()
+	else
+		-- Normal buffer
+		target_path = vim.api.nvim_buf_get_name(0)
+	end
+
+	local git_root = utils.get_git_root()
+	if not git_root then
+		print("Error: not inside a Git repository")
+		return
+	end
+	local rel_path = target_path:sub(#git_root + 2)
+	local remote_url = utils.get_git_remote()
+	local commit = utils.get_commit_hash()
+
+	-- Extract project name from remote URL (supports https and ssh)
+	local package_name = utils.extract_repo_path(remote_url)
+	-- local start_line, end_line = get_visual_range()
+	local line_fragment = ""
+	local start_line, end_line = utils.get_visual_range()
+	if not utils.is_directory_buffer() then
+		line_fragment = (start_line == end_line) and ("#L" .. start_line) or ("#L" .. start_line .. "-L" .. end_line)
+	end
+
+	local url = config.link_builder({
+		repo = package_name,
+		commit = commit,
+		rel_path = rel_path,
+		start_line = start_line,
+		end_line = end_line,
+	})
+
+	vim.fn.setreg("+", url)
+	print("GitFarm URL copied to clipboard: " .. url)
+end
+
+function ShareGitLink.setup(opts)
+	config.link_builder = opts and opts.link_builde or utils.default_link_builder
+end
+
+return ShareGitLink
---
+++ lua/utils.lua
@@ -0,0 +1,90 @@
+local M = {}
+
+local function safe_read_command(cmd_args)
+	local cwd = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p:h")
+	local result = vim.system(cmd_args, { cwd = cwd }):wait()
+	if result.code ~= 0 then
+		return nil
+	end
+	return vim.fn.trim(result.stdout)
+end
+
+function M.get_git_root()
+	return safe_read_command({ "git", "rev-parse", "--show-toplevel" })
+end
+
+function M.get_git_remote()
+	return safe_read_command({ "git", "remote", "get-url", "origin" })
+end
+
+function M.get_commit_hash()
+	return safe_read_command({ "git", "rev-parse", "HEAD" })
+end
+
+function M.is_directory_buffer()
+	local name = vim.api.nvim_buf_get_name(0)
+	return name ~= "" and vim.fn.isdirectory(name) == 1
+end
+
+function M.get_selected_tree_path()
+	local cwd = vim.fn.getcwd()
+	local target_file = vim.fn.expand("<cfile>")
+
+	if target_file == "" then
+		-- fallback to current directory
+		return cwd
+	end
+
+	local current_directory = vim.api.nvim_buf_get_name(0)
+
+	return vim.fn.resolve(current_directory .. "/" .. target_file)
+end
+
+function M.get_visual_range()
+	local mode = vim.fn.mode()
+	if mode ~= "v" and mode ~= "V" then
+		local line = vim.fn.line(".")
+		return line, line
+	end
+
+	local start_line = vim.fn.line("v")
+	local end_line = vim.fn.line(".")
+	if start_line > end_line then
+		start_line, end_line = end_line, start_line
+	end
+	return start_line, end_line
+end
+
+function M.extract_repo_path(remote_url)
+	-- Remove trailing .git
+	remote_url = remote_url:gsub("%.git$", "")
+
+	-- SSH format: git@host:owner/repo
+	local ssh_path = remote_url:match("git@[^:]+:(.+)")
+	if ssh_path then
+		return ssh_path
+	end
+
+	-- HTTPS format: https://host/owner/repo
+	local https_path = remote_url:match("https?://[^/]+/(.+)")
+	if https_path then
+		return https_path
+	end
+
+	-- Fallback: just the last segment (best guess)
+	return remote_url:match("([^/]+)$")
+end
+
+function M.default_link_builder(opts)
+	local url = string.format("https://github.com/%s/blob/%s/%s", opts.repo, opts.commit, opts.rel_path)
+	if opts.start_line then
+		if opts.end_line and opts.end_line ~= opts.start_line then
+			url = url .. string.format("#L%d-L%d", opts.start_line, opts.end_line)
+		else
+			url = url .. string.format("#L%d", opts.start_line)
+		end
+	end
+	return url
+end
+
+return M
